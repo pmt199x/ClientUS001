43eeb3bf69086d60508721456053d5c6b2ed8c88
Pham Minh Tri <pmt199x@gmail.com>
2021-03-09 16:37:37 +0700

    setup project

---
+++ src/main/java/controller/ClientController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClientController {
+
+	@RequestMapping("/")
+	public String Index() {
+		return "user/index";
+	}
+}
